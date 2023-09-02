ffdc8e458cf898068ee8f83bf576dac2d233f1bc
atzina <atzina@gmail.com>
2023-09-01 21:17:54 -0600

    efecto de la imagen del fondo

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,37 +1,26 @@
 import './Home.css';
-import Water from '../../assets/agua_grande.jpg';
-import React,{ useState} from 'react';
+
 
 function Home() {
 
-  const[scrolling, setScrolling] = useState(false);
-
-   // Función para manejar el evento de desplazamiento
-   const handleScroll = () => {
-    if (window.scrollY > 50) {
-      setScrolling(true);
-    } else {
-      setScrolling(false);
-    }
-  };
-
-      // Agregar el oyente de eventos cuando se monta el componente
-  React.useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
   return (
     <>
-      <h1>HOME</h1>
-      <div className="background-image">
-      <img className="img-agua" src={Water}></img>
+      <div className='title'>
+        <h1>HOME</h1>
       </div>
-      <div className={`content ${scrolling ? 'scrolling' : ''}`}>
-        <h2>Cotenido de abajo</h2>
-      </div>
+      
+      <body>
+        <div className="vacio">
+        </div>
+        
+        <div className='contenedor'>
+          <div className='contenido'>
+            <h2>Cotenido de abajo</h2>
+          </div>
+          
+        </div>
+      </body>
     </>
   );
 }
