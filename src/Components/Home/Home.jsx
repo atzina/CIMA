babe133dc630db706d150a778fbc4b3181bba6ae
atzina <atzina@gmail.com>
2023-09-07 19:29:18 -0600

    primer avance secciones + fotos seccion tres

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,7 @@
 import './Home.css';
+import imageone from '../../assets/gota.jpg';
+import imagetwo from '../../assets/platas2.jpg';
+import imagethree from '../../assets/laboratorio.jpg';
 
 
 function Home() {
@@ -21,8 +24,25 @@ function Home() {
           <div className='wave'>
             <div className='contenido'>
               <h2>Cotenido de abajo</h2>
+              <p>somos una empresa con más de 20 años de experiencia en Saneamiento</p>
             </div>
           </div>
+          <div className='div-seccion-dos'>
+            <div className='cont-img-uno'>
+               <img className= 'imagen-uno' src = {imageone}></img>
+            </div>
+            <div className='cont-img-dos'>
+              <img className='imagen-dos'src ={imagetwo}></img>
+            </div>
+            <div className='cont-img-tres'>
+              <img className='imagen-tres' src = {imagethree}></img>
+            </div>
+            
+          </div>
+          <div className='div-contacto'>
+            <h1>Aquí haz contacto</h1>
+          </div>
+
           
         </div>
       </body>
